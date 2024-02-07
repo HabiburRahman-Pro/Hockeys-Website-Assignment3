3766382acbf931ebb5678cfc5465fc2ebec1e38d
HabiburRahman-Pro <habibur426746@gmail.com>
2024-02-07 23:25:51 +0600

    Header and main(half) section added

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+    current: 'currentColor',
+    'white': '#ffffff',
+    'purple': '#3f3cbb',
+    'midnight': '#121063',
+}
+module.exports = {
+    //...
+    daisyui: {
+        themes: ["light", "dark", "cupcake"],
+    },
+}
